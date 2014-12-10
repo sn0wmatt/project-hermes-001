3baeb39119021d3c16621076e43843fdca3702e0
Matthew Carr <221297.Matt@gmail.com>
2014-12-10 18:55:10 +1100

    Changed repeated='true' -> repeated=True
    Fixed a TypeError in the Talk class.

--- models.py
+++ models.py
@@ -2,6 +2,6 @@ from google.appengine.ext import ndb
 
 class Talk(ndb.Model):
     name = ndb.StringProperty()
-    users = ndb.UserProperty(repeated='true')
-    messages = ndb.JsonProperty(repeated='true')
+    users = ndb.UserProperty(repeated=True)
+    messages = ndb.JsonProperty(repeated=True)
     host = ndb.UserProperty()
